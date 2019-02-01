79c5bf9bbb3b8abe7fbc4aa67956f8ddf623f300
Mirko Westermeier <mirko@westermeier.de>
2019-02-01 17:50:12 +0100

    Write out command documentation to match definitions in PR #3

--- lib/ViralGiral/Command.pm
+++ lib/ViralGiral/Command.pm
@@ -16,10 +16,34 @@ sub usage_with_thing ($self, $thing, $things) {
     <<"USAGE"
 Usage: APPLICATION vg_$thing [OPTIONS] [UUID]
 
+    ./app vg_$thing UUID
     ./app vg_$thing --list --sort=name
-    ./app vg_$thing --add "name => 'Foo', color => 'white'"
+    ./app vg_$thing --create "name => 'Foo', color => 'white'"
     ./app vg_$thing --update "answer => 42" UUID
     ./app vg_$thing --delete UUID
+
+Runmode list (--list [--sort X] [--show Y] [UUID])
+
+    - List all $things sorted by creation time.
+    - List all $things sorted by data field "foo" with --sort foo.
+    - Explicitly add data field "bar" to listing with --show bar.
+    - Complete dump of all data of the $thing when a UUID is given.
+    - The list runmode is default, so it doesn't need to be typed.
+
+Runmode create (--create DATA_STRING)
+
+    - Create a new $thing, the created UUID is printed back.
+    - Interprete the given DATA_STRING as the new data hash.
+    - Print the UUID of the created $thing.
+
+Runmode update (--update DATA_STRING UUID)
+
+    - Update the $thing with the given UUID.
+    - Use the DATA_STRING to update single data hash values.
+
+Runmode delete (--delete UUID)
+
+    - Delete the $thing with the given UUID. (Caution!)
 USAGE
 }
 
